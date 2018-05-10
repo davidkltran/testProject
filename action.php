d9cd1bffb055029e6752e4eb10c13287ef68a061
davidkltran <davidkltran@gmail.com>
2018-05-10 03:36:31 -0700

    created php files

---
+++ action.php
@@ -0,0 +1,111 @@
+<?php
+
+include "db.php";
+
+class DataOperation extends Database
+{
+    public function insert_record($table,$fileds){
+        //"INSERT INTO table_name ( ) VALUES ('c_name','c_email','c_phone')";
+        $sql = ""; 
+        $sql .= "INSERT INTO ".$table;
+        $sql .= " (".implode(",", array_keys($fileds)).") VALUES ";
+        $sql .= "('".implode("','", array_values($fileds))."')";
+        $query = mysqli_query($this->con,$sql);
+        if($query){
+            return true;
+        }
+
+    }
+    public function fetch_record($table){
+        $sql = "SELECT * FROM ".$table;
+        $array = array();
+        $query = mysqli_query($this->con,$sql);
+        while($row = mysqli_fetch_assoc($query)){
+            $array[] = $row;
+        }
+        return $array;
+    }
+    public function select_record($table,$where){
+        $sql = "";
+        $condition = "";
+        foreach ($where as $key => $value) {
+            // id = '5' AND c_name = 'something'
+            $condition .= $key . "='" . $value . "' AND ";
+        }
+        $condition = substr($condition, 0, -5);
+        $sql .= "SELECT * FROM ".$table." WHERE ".$condition;
+        $query = mysqli_query($this->con,$sql);
+        $row = mysqli_fetch_array($query);
+        return $row;
+    }
+    public function update_record($table,$where,$fields){
+        $sql = "";
+        $condition = "";
+        foreach ($where as $key => $value) {
+            // id = '5' AND c_name = 'something'
+            $condition .= $key . "='" . $value . "' AND ";
+        }
+        $condition = substr($condition, 0, -5);
+        foreach ($fields as $key => $value) {
+            //UPDATE table SET c_name = '', qty = '' WHERE id = '';
+            $sql .= $key . "='".$value."', ";
+        }
+        $sql = substr($sql, 0, -2);
+        $sql = "UPDATE ".$table." SET ".$sql." WHERE ".$condition;
+        if(mysqli_query($this->con,$sql)){
+            return true;
+        }
+    }
+    public function delete_record($table,$where){
+        $sql = "";
+        $condition = "";
+        foreach ($where as $key => $value) {
+            $condition .= $key . "='" . $value . "' AND ";
+        }
+        $condition = substr($condition, 0, -5);
+        $sql = "DELETE FROM ".$table." WHERE ".$condition;
+        if(mysqli_query($this->con,$sql)){
+            return true;
+        }
+    }
+}
+
+$obj = new DataOperation;
+
+
+if(isset($_POST["submit"])){
+    $myArray = array(
+        "t_name" => $_POST["name"],
+        "t_email" => $_POST["email"],
+        "t_phone" => $_POST["phone"]
+        );
+    if($obj->insert_record("testdb",$myArray)){
+        header("location:index.php?msg=Record Inserted");
+    }
+   
+}
+
+if(isset($_POST["edit"])){
+    $id = $_POST["id"];
+    $where = array("id"=>$id);
+    $myArray = array(
+        "t_name" => $_POST["name"],
+        "t_email" => $_POST["email"],
+        "t_phone" => $_POST["phone"]
+        );
+    if($obj->update_record("testdb",$where,$myArray)){
+        header("location:index.php?msg=Record Updated Successfully");
+    }
+   
+}
+
+if(isset($_GET["delete"])){
+    $id = $_GET["id"] ?? NULL;
+    $where = array("id"=>$id);
+    if($obj->delete_record("testdb",$where)){
+        header("location:index.php?msg=Record Deleted Successfully");
+    }
+    
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,141 @@
+<?php
+
+include "action.php";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<script scr="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<script scr="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script><meta http-equiv="Content-
+Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width-device-width,initial-scale=1.0"/>
+<title>Your website</title>
+<link rel="stylesheet" href="" type="text/css" />
+<script type="text/javascript"></script>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Contact Management <small>IS445</small></h1>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Enter Contact Details</div>
+                    <div class="panel-body">
+                        <?php
+                            if(isset($_GET["update"])){
+                                //php 7
+                                $id = $_GET["id"] ?? null;
+                                $where = array("id"=>$id,);
+                                $row = $obj->select_record("contacts",$where);
+                                ?>
+                                       <form method="post" action="action.php">
+                                            <table class="table table-hover">
+                                                <tr>
+                                                    <td><input type="hidden" name="id" value="<?php echo $id;  ?>"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Name</td>
+                                                    <td><input type="text" class="form-control" values="<?php echo $row["t_name"]; ?>" name="name" placeholder="Enter New Name"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Email</td>
+                                                    <td><input type="text" class="form-control" name="email" values="<?php echo $row["t_email"]; ?>" placeholder="Enter New Email"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Phone</td>
+                                                    <td><input type="text" class="form-control" name="phone" values="<?php echo $row["t_phone"]; ?>" placeholder="Enter New Phone"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td colspan="2" align="center"><input type="submit" class="btn btn-primary" name="edit" value="Update"></td>
+                                                </tr>
+                                            </table>
+                                        </form>
+
+                                <?php
+                            }else{
+                                ?>
+                                    <form method="post" action="action.php">
+                                        <table class="table table-hover">
+                                            <tr>
+                                                <td>Name</td>
+                                                <td><input type="text" class="form-control" name="name" placeholder="Enter Name"></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Email</td>
+                                                <td><input type="text" class="form-control" name="email" placeholder="Enter Email"></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Phone</td>
+                                                <td><input type="text" class="form-control" name="phone" placeholder="Enter Phone"></td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="2" align="center"><input type="submit" class="btn btn-primary" name="submit" value="Store"></td>
+                                            </tr>
+                                        </table>
+                                    </form>
+                                
+                                <?php
+                                }
+                        
+                        
+                        ?>
+                        
+                </div>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+       
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class "col-md-2"></div>
+            <div class "col-md-8">
+                <table class="table table-bordered">
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>&nbsp;</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                    <?php
+                        $myrow = $obj->fetch_record("testdb");
+                        foreach ($myrow as $row){
+                            //breaking point
+                            ?>
+                                <tr>
+                                    <td><?php echo $row["id"]; ?></td>
+                                    <td><?php echo $row["t_name"]; ?></td>
+                                    <td><?php echo $row["t_email"]; ?></td>
+                                    <td><b><?php echo $row["t_phone"]; ?></b></td>
+                                    <td><a href="index.php?update=1&id=<?php echo $row["id"]; ?>"class="btn btn-info">Edit</a></td>
+                                    <td><a href="action.php?delete=1&id=<?php echo $row["id"]; ?>" class="btn btn-danger">Delete</a></td>
+                                </tr>
+                            
+                            
+                            <?php
+                        }
+                    ?>
+                    
+                </table>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+    </div>
+
+</body>
+</html>
